902baa8434b80284a70a818245f5477e15d1ecef
alexandra david <alexdav13@gmail.com>
2024-08-05 13:51:39 +0200

    ex 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,15 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+
+    if @question.nil?
+      @answer = "You didn't ask anything!"
+    elsif @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
